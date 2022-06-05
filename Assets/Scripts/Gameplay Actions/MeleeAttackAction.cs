63f8432fde14603f18f6dfec7de6a13eb2336380
Andrew Barnes <55672451+LividVengeance@users.noreply.github.com>
2022-06-06 01:25:06 +1200

    Update To Latest LTS 2021.3.4f

--- Assets/Scripts/Gameplay Actions/MeleeAttackAction.cs
+++ Assets/Scripts/Gameplay Actions/MeleeAttackAction.cs
@@ -7,8 +7,29 @@ public class MeleeAttackAction : AttackAction
     public MeleeAttackAction(ActionSystemComponent ActionSystemComponent, GameObject OwningGameObject) 
         : base(ActionSystemComponent, OwningGameObject) { }
 
+    public enum EColliderType
+    {
+        EBoxCollider,
+        ECircleCollider,
+    };
+
+    public enum EAimType
+    {
+        ECursorPosition,
+        ECursorDirection,
+        EPlayerPosition,
+        EPlayerDirection,
+        EClosestEnemyPosition,
+    };
+
+    public EColliderType ColliderType;
+    public EAimType AimType;
+    public Collider2D Collider2D;
+
     public override void StartAction()
     {
+        base.StartAction();
+        Collider2D = ColliderType == EColliderType.EBoxCollider ? new BoxCollider2D() : new CircleCollider2D();
         //Vector3 MousePositon = GetMouseWorldPosition(Input.mousePosition, Camera.main);
         //Vector3 AttackDirection = (MousePositon - GetOwningCharacter().transform.position);
         //
@@ -17,5 +38,10 @@ public class MeleeAttackAction : AttackAction
         //    Debug.Log(AttackDirection);
         //    Debug.DrawRay(GetOwningCharacter().transform.position, AttackDirection, Color.magenta, 3.0f);
         //}
+    }
+
+    public void OpenCollisionDetection()
+    {
+
     }
 }
--- Assets/Scripts/UI/GameHUD.cs
+++ Assets/Scripts/UI/GameHUD.cs
@@ -19,24 +19,9 @@ public class GameHUD : MonoBehaviour
     [SerializeField] private GameObject InventoryUIGameobject;
     [SerializeField] private GameObject InventoryUIConentGameobject;
 
-    [Header("Flash UI ")]
-    [SerializeField] private Image OffBeatFlash;
-    [SerializeField] private Image OnBeatFlash;
-    [SerializeField] private float FlashTime = 0.1f;
-    [SerializeField] private bool bEnableFlash = true;
-
-    // Flash
-    private readonly Color OnBeatFlashColour = new Vector4(1, 1, 1, .05f);
-    private readonly Color OffBeatFlashColour = new Vector4(1, 0, 0, .5f);
-    private readonly Color NoFlashColour = new Vector4(1, 1, 1, 0);
-    private Coroutine OnBeatFlashCoroutine = null;
-    private Coroutine OffBeatFlashCoroutine = null;
-
 
     private void Start()
     {
-        TempoManager.Instance.GetBeatUnityEvent().AddListener(OnBeat);
-        TempoManager.Instance.GetActionOffBeatUnityEvent().AddListener(OffBeat);
         InputManager = TempoManager.Instance.GetInputManager();
     }
 
@@ -61,23 +46,6 @@ public class GameHUD : MonoBehaviour
                 InputManager.EnableCursor();
             }
         }
-    }
-
-    private void OnBeat()
-    {
-        if (bEnableFlash) OnBeatFlashCoroutine = StartCoroutine(FlashActionColour(OnBeatFlashColour, OnBeatFlash));
-    }
-
-    private void OffBeat()
-    {
-        if (bEnableFlash) OffBeatFlashCoroutine = StartCoroutine(FlashActionColour(OffBeatFlashColour, OffBeatFlash));
-    }
-
-    private IEnumerator FlashActionColour(Color FlashColour, Image FlashImage)
-    {
-        FlashImage.color = FlashColour;
-        yield return (new WaitForSeconds(FlashTime));
-        FlashImage.color = NoFlashColour;
     }
 
     public GameObject GetInventoryUIContent() => InventoryUIConentGameobject;
